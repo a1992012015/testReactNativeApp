74868916a070dc99741010a887aa4d508d767c4f
刘杰-HOME <mengyehuanyu@qq.com>
2018-05-05 00:52:41 +0800

    Signed-off-by: 刘杰-HOME <mengyehuanyu@qq.com>测试热更新打包和更新的流程

--- android/app/src/main/java/com/testreactnativeapp/MainApplication.java
+++ android/app/src/main/java/com/testreactnativeapp/MainApplication.java
@@ -3,10 +3,12 @@ package com.testreactnativeapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cn.reactnative.modules.update.UpdatePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import cn.reactnative.modules.update.UpdateContext;//热更新添加
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +16,11 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+      @Override
+      protected String getJSBundleFile() {
+          return UpdateContext.getBundleUrl(MainApplication.this);
+      }
+
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -22,7 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new UpdatePackage()
       );
     }
 
